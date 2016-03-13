c37ee947ad677bdee782f5693ed9f59267f68338
Glen Starchman <glen@starchman.com>
2016-03-13 08:48:06 -0700

    cleanup

--- scripts/deploy_images.sh
+++ scripts/deploy_images.sh
@@ -1,4 +1,4 @@
 ROOT_DIR="."
 CLASS_PATH="$(echo $ROOT_DIR/lib/*.jar | tr ' ' ':'):$ROOT_DIR/config"
-java -cp $CLASS_PATH com.didd.common.Deploy image_upload public/img images
+java -cp $CLASS_PATH starman.common.Deploy image_upload public/img images
 
--- scripts/generate_static.sh
+++
@@ -1,4 +0,0 @@
-ROOT_DIR="."
-CLASS_PATH="$(echo $ROOT_DIR/lib/*.jar | tr ' ' ':'):$ROOT_DIR/config"
-
-java -cp $CLASS_PATH com.didd.common.StaticGenerator
